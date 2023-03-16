d7af91bf543df3792ece992a60ad9d97c1fd8883
Emanuel <lejiei@gmail.com>
2023-03-16 06:59:49 -0400

    first commit estructure

---
+++ netcoreTodoList/netcoreTodoList/Context/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿namespace netcoreTodoList.Context
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TodoContext
+        : DbContext
+    {
+        public TodoContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ netcoreTodoList/netcoreTodoList/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿namespace netcoreTodoList.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Task")]
+    public class Task
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        
+
+        public string TaskName { get; set; }
+
+        public string DeadLine { get; set; }    
+
+        
+    }
+}
